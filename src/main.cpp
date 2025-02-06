871a73bb796ee0964d5d1c42d34e89dcb9bdb5d6
ivan <ivanpdoria@gmail.com>
2025-02-06 20:50:08 -0300

    Add code to connect to wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,53 @@
-/*
-  Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
-*/
-
 #include <Arduino.h>
 #include <ArduinoOTA.h>
+#include <Wire.h>
+
+#include "secrets.h"
+
+#define LED_BUILTIN 2
+
+const char *ssid = WIFI_SSID;
+const char *password = WIFI_PASSWORD;
 
 // the setup function runs once when you press reset or power the board
 void setup()
 {
   // initialize digital pin LED_BUILTIN as an output.
-  // pinMode(LED_BUILTIN, OUTPUT);
-  // Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH); // turn the LED off by making the voltage LOW
+
+  Serial.begin(115200);
+
+  bool led_state = LOW;
+
+  Serial.println("Scanning for WiFi networks...");
+
+  Serial.print("SSID: ");
+  Serial.println(WIFI_SSID);
+  Serial.print("Password: ");
+  Serial.println(WIFI_PASSWORD); // Be careful with this!
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+    digitalWrite(LED_BUILTIN, led_state);
+    led_state = !led_state;
+  }
+  Serial.println("\nConnected to WiFi");
+
+  delay(2000);
 }
 
 // the loop function runs over and over again forever
 void loop()
 {
-  // Serial.println("LED ON");
-  // digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
-  // delay(1000);
-  // Serial.println("LED OFF");
-  // digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
-  // delay(1000);                    // wait for a second
+  Serial.println("LED ON");
+  digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+  delay(2000);
+  Serial.println("LED OFF");
+  digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
+  delay(2000);                    // wait for a second
 }
